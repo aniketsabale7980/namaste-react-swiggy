44d9bfc65fbc21ab274e264428482b9dc205801c
aniketsabale <aniketsabale07@gmail.com>
2025-02-01 01:08:11 +0530

    lecture 5 completed

--- namaste-react/src/App.js
+++ namaste-react/src/App.js
@@ -4,10 +4,566 @@ const App = () => {
   return <AppLayout />;
 };
 
+const restaurantList = [
+  {
+    card: {
+      card: {
+        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        info: {
+          id: "506982",
+          name: "McDonald's",
+          cloudinaryImageId:
+            "RX_THUMBNAIL/IMAGES/VENDOR/2024/9/18/a6317ac5-d089-45bf-a392-c5384cbdd1e8_506982.jpg",
+          locality: "Narayan Peth",
+          areaName: "Shaniwar Peth",
+          costForTwo: "₹400 for two",
+          cuisines: ["Burgers", "Beverages"],
+          avgRating: 4.6,
+          parentId: "630",
+          avgRatingString: "4.6",
+          totalRatingsString: "2.1K+",
+          sla: {
+            deliveryTime: 43,
+            lastMileTravel: 1.3,
+            serviceability: "SERVICEABLE",
+            slaString: "40-45 mins",
+            lastMileTravelString: "1.3 km",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "2025-02-01 00:45:00",
+            opened: true,
+          },
+          badges: {},
+          isOpen: true,
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              textBased: {},
+              imageBased: {},
+              textExtendedBadges: {},
+            },
+          },
+          aggregatedDiscountInfoV3: {
+            header: "₹200 OFF",
+            subHeader: "ABOVE ₹799",
+            discountTag: "FLAT DEAL",
+            logoCtx: {
+              text: "BENEFITS",
+            },
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+            commsStyling: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          restaurantOfferPresentationInfo: {},
+          externalRatings: {
+            aggregatedRating: {
+              rating: "--",
+            },
+          },
+          ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+        },
+        analytics: {},
+        cta: {
+          link: "swiggy://menu?restaurant_id=506982&source=collection&query=Burger",
+          text: "RESTAURANT_MENU",
+          type: "DEEPLINK",
+        },
+        widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food",
+      },
+      relevance: {
+        type: "RELEVANCE_TYPE_ON_MENU_RETURN",
+        sectionId: "MENU_RETURN_FOOD",
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        info: {
+          id: "934639",
+          name: "B Burger - Bigger Burger",
+          cloudinaryImageId:
+            "FOOD_CATALOG/IMAGES/CMS/2024/4/2/08fd14ff-fe06-4778-b25f-cfe1a8a09cf4_cc5b070d-c307-40ea-a9e6-e1051b828573.jpg",
+          locality: "Kothrud",
+          areaName: "Kothrud",
+          costForTwo: "₹500 for two",
+          cuisines: ["Burgers", "Fast Food", "American", "Beverages"],
+          avgRating: 4.4,
+          parentId: "454391",
+          avgRatingString: "4.4",
+          totalRatingsString: "200",
+          promoted: true,
+          adTrackingId:
+            "cid=24295646~p=1~adgrpid=24295646#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=934639~eid=6d45d8c6-532f-45d6-a1c3-e7f74f90defb~srvts=1738348628440~collid=83637",
+          sla: {
+            deliveryTime: 38,
+            lastMileTravel: 6.8,
+            serviceability: "SERVICEABLE",
+            slaString: "35-40 mins",
+            lastMileTravelString: "6.8 km",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "2025-02-01 03:45:00",
+            opened: true,
+          },
+          badges: {
+            imageBadges: [
+              {
+                imageId: "newg.png",
+                description: "Gourmet",
+              },
+            ],
+          },
+          isOpen: true,
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              textBased: {},
+              imageBased: {
+                badgeObject: [
+                  {
+                    attributes: {
+                      imageId: "newg.png",
+                      description: "Gourmet",
+                    },
+                  },
+                ],
+              },
+              textExtendedBadges: {},
+            },
+          },
+          aggregatedDiscountInfoV3: {
+            header: "ITEMS",
+            subHeader: "AT ₹199",
+            logoCtx: {
+              text: "BENEFITS",
+            },
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+            commsStyling: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          restaurantOfferPresentationInfo: {},
+          externalRatings: {
+            aggregatedRating: {
+              rating: "--",
+            },
+          },
+          ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+          campaignId: "24295646",
+        },
+        analytics: {},
+        cta: {
+          link: "swiggy://menu?restaurant_id=934639&source=collection&query=Burger",
+          text: "RESTAURANT_MENU",
+          type: "DEEPLINK",
+        },
+        widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food",
+      },
+      relevance: {
+        type: "RELEVANCE_TYPE_ON_MENU_RETURN",
+        sectionId: "MENU_RETURN_FOOD",
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        info: {
+          id: "506984",
+          name: "McDonald's Gourmet Burger Collection",
+          cloudinaryImageId: "byhsz033wskdgxxjymk8",
+          locality: "Narayan Peth",
+          areaName: "Swargate",
+          costForTwo: "₹400 for two",
+          cuisines: ["Burgers", "Beverages", "Cafe", "Desserts"],
+          avgRating: 4.4,
+          parentId: "10761",
+          avgRatingString: "4.4",
+          totalRatingsString: "32",
+          sla: {
+            deliveryTime: 33,
+            lastMileTravel: 1.3,
+            serviceability: "SERVICEABLE",
+            slaString: "30-35 mins",
+            lastMileTravelString: "1.3 km",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "2025-02-01 00:45:00",
+            opened: true,
+          },
+          badges: {},
+          isOpen: true,
+          aggregatedDiscountInfoV2: {},
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              textBased: {},
+              imageBased: {},
+              textExtendedBadges: {},
+            },
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+            commsStyling: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          restaurantOfferPresentationInfo: {},
+          externalRatings: {
+            aggregatedRating: {
+              rating: "--",
+            },
+          },
+          ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+        },
+        analytics: {},
+        cta: {
+          link: "swiggy://menu?restaurant_id=506984&source=collection&query=Burger",
+          text: "RESTAURANT_MENU",
+          type: "DEEPLINK",
+        },
+        widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food",
+      },
+      relevance: {
+        type: "RELEVANCE_TYPE_ON_MENU_RETURN",
+        sectionId: "MENU_RETURN_FOOD",
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        info: {
+          id: "721793",
+          name: "Munch Mates",
+          cloudinaryImageId:
+            "FOOD_CATALOG/IMAGES/CMS/2024/7/3/987fff3a-bb96-47e8-baef-4f16fb2a96b5_23cfcf60-5c05-4a49-bdba-2de65e2f3a61.JPG",
+          locality: "Nkarve Road",
+          areaName: "Kothrud",
+          costForTwo: "₹200 for two",
+          cuisines: ["Indian"],
+          avgRating: 4.2,
+          parentId: "429844",
+          avgRatingString: "4.2",
+          totalRatingsString: "161",
+          promoted: true,
+          adTrackingId:
+            "cid=24469184~p=3~adgrpid=24469184#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=721793~eid=5da666c9-c351-4b00-a796-3d6b2e52a08d~srvts=1738348628440~collid=83637",
+          sla: {
+            deliveryTime: 42,
+            lastMileTravel: 7,
+            serviceability: "SERVICEABLE",
+            slaString: "40-45 mins",
+            lastMileTravelString: "7.0 km",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "2025-02-01 23:59:00",
+            opened: true,
+          },
+          badges: {},
+          isOpen: true,
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              textExtendedBadges: {},
+              textBased: {},
+              imageBased: {},
+            },
+          },
+          aggregatedDiscountInfoV3: {
+            header: "40% OFF",
+            subHeader: "UPTO ₹80",
+            logoCtx: {
+              text: "BENEFITS",
+            },
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+            commsStyling: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          restaurantOfferPresentationInfo: {},
+          externalRatings: {
+            aggregatedRating: {
+              rating: "4.2",
+              ratingCount: "110",
+            },
+            source: "GOOGLE",
+            sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+          },
+          ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+          campaignId: "24469184",
+        },
+        analytics: {},
+        cta: {
+          link: "swiggy://menu?restaurant_id=721793&source=collection&query=Burger",
+          text: "RESTAURANT_MENU",
+          type: "DEEPLINK",
+        },
+        widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food",
+      },
+      relevance: {
+        type: "RELEVANCE_TYPE_ON_MENU_RETURN",
+        sectionId: "MENU_RETURN_FOOD",
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        info: {
+          id: "42581",
+          name: "Burger King",
+          cloudinaryImageId:
+            "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/55d0daa1-ad2e-4893-be06-4709c5c68d49_42581.JPG",
+          locality: "The Pavillion Mall",
+          areaName: "Shivajinagar",
+          costForTwo: "₹350 for two",
+          cuisines: ["Burgers", "American"],
+          avgRating: 4.1,
+          parentId: "166",
+          avgRatingString: "4.1",
+          totalRatingsString: "13K+",
+          sla: {
+            deliveryTime: 44,
+            lastMileTravel: 4.5,
+            serviceability: "SERVICEABLE",
+            slaString: "40-45 mins",
+            lastMileTravelString: "4.5 km",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "2025-02-01 02:00:00",
+            opened: true,
+          },
+          badges: {},
+          isOpen: true,
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              textBased: {},
+              imageBased: {},
+              textExtendedBadges: {},
+            },
+          },
+          aggregatedDiscountInfoV3: {
+            header: "60% OFF",
+            subHeader: "UPTO ₹120",
+            logoCtx: {
+              text: "BENEFITS",
+            },
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+            commsStyling: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          restaurantOfferPresentationInfo: {},
+          externalRatings: {
+            aggregatedRating: {
+              rating: "--",
+            },
+          },
+          ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+        },
+        analytics: {},
+        cta: {
+          link: "swiggy://menu?restaurant_id=42581&source=collection&query=Burger",
+          text: "RESTAURANT_MENU",
+          type: "DEEPLINK",
+        },
+        widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food",
+      },
+      relevance: {
+        type: "RELEVANCE_TYPE_ON_MENU_RETURN",
+        sectionId: "MENU_RETURN_FOOD",
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        info: {
+          id: "675530",
+          name: "First Class Pizza Burger",
+          cloudinaryImageId:
+            "FOOD_CATALOG/IMAGES/CMS/2024/10/24/64f85819-aa16-4d55-8400-58382c081b9d_cb76e88b-2921-4494-b8ce-95806b246b39.JPG",
+          locality: "Karve Nagar",
+          areaName: "Kothrud",
+          costForTwo: "₹400 for two",
+          cuisines: ["Pizzas", "Burgers", "Snacks", "Beverages"],
+          avgRating: 4.1,
+          parentId: "404080",
+          avgRatingString: "4.1",
+          totalRatingsString: "114",
+          promoted: true,
+          adTrackingId:
+            "cid=24468797~p=4~adgrpid=24468797#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=675530~eid=95a69b7e-e657-48d8-b837-057128285007~srvts=1738348628440~collid=83637",
+          sla: {
+            deliveryTime: 38,
+            lastMileTravel: 7,
+            serviceability: "SERVICEABLE",
+            slaString: "35-40 mins",
+            lastMileTravelString: "7.0 km",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "2025-02-07 00:00:00",
+            opened: true,
+          },
+          badges: {},
+          isOpen: true,
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              textBased: {},
+              imageBased: {},
+              textExtendedBadges: {},
+            },
+          },
+          aggregatedDiscountInfoV3: {
+            header: "40% OFF",
+            subHeader: "UPTO ₹80",
+            logoCtx: {
+              text: "BENEFITS",
+            },
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+            commsStyling: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          restaurantOfferPresentationInfo: {},
+          externalRatings: {
+            aggregatedRating: {
+              rating: "--",
+            },
+          },
+          ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+          campaignId: "24468797",
+        },
+        analytics: {},
+        cta: {
+          link: "swiggy://menu?restaurant_id=675530&source=collection&query=Burger",
+          text: "RESTAURANT_MENU",
+          type: "DEEPLINK",
+        },
+        widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food",
+      },
+      relevance: {
+        type: "RELEVANCE_TYPE_ON_MENU_RETURN",
+        sectionId: "MENU_RETURN_FOOD",
+      },
+    },
+  },
+];
+
+const IMN_CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+const RestaurantCard = ({ name, cloudinaryImageId, cuisines, costForTwo }) => {
+  // console.log("Rastaurant --->>", props);
+  // const {  } = props.restaurant;
+  return (
+    <div className="card">
+      <img alt="card" src={IMN_CDN_URL + cloudinaryImageId} />
+      <h2>{name}</h2>
+      <h3>{cuisines.join(", ")}</h3>
+      <h4>{costForTwo}</h4>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <>
       <HeaderComponent />
+      <Body />
+      <Footer />
     </>
   );
 };
@@ -18,7 +574,7 @@ const Title = () => {
       <img
         className="logo"
         alt="logo"
-        src="https://static.vecteezy.com/system/resources/previews/013/195/659/non_2x/restaurant-delicious-food-logo-badge-line-style-design-with-smile-face-fork-and-spoon-icon-concept-for-catering-food-culinary-logo-design-vector.jpg"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0NDG_SR77n8xtaRwNksZyk1oFmSQiQKBN8X4OQj68dwNc8hjWh3QI3qA&s"
       />
     </h1>
   );
@@ -29,7 +585,7 @@ const HeaderComponent = () => {
     <>
       <div className="nav-item">
         <Title />
-        <ul className="">
+        <ul>
           <li>Home</li>
           <li>About</li>
           <li>Contact</li>
@@ -38,6 +594,33 @@ const HeaderComponent = () => {
       </div>
     </>
   );
+};
+
+const Body = () => {
+  return (
+    <div className="restaurant-list">
+      {restaurantList.map((restro) => {
+        return (
+          <RestaurantCard
+            {...restro.card.card.info}
+            key={restro.card.card.info.id}
+          />
+        );
+      })}
+    </div>
+  );
+  // <div className="restaurant-list">
+  //   <RestaurantCard />
+  //   <RestaurantCard />
+  //   <RestaurantCard />
+  //   <RestaurantCard />
+  //   <RestaurantCard />
+  //   <RestaurantCard />
+  // </div>
+};
+
+const Footer = () => {
+  return <div>Footer</div>;
 };
 
 export default App;
