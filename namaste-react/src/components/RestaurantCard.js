1573f56e7dfb7ec1d13a29b40fa90da3444655d6
aniketsabale <aniketsabale07@gmail.com>
2025-02-09 12:55:42 +0530

    tailwind implementation

--- namaste-react/src/components/RestaurantCard.js
+++ namaste-react/src/components/RestaurantCard.js
@@ -2,10 +2,10 @@ import { IMN_CDN_URL } from "../Constants";
 
 const RestaurantCard = ({ name, cloudinaryImageId, avgRating, costForTwo }) => {
   return (
-    <div className="card">
+    <div className="w-56 p-2 m-2 shadow-lg bg-pink-100">
       <img alt="card" src={IMN_CDN_URL + cloudinaryImageId} />
-      <h2>{name}</h2>
-      <h3>{avgRating}</h3>
+      <h2 className="text-xl font-bold">{name}</h2>
+      <h3 cla>{avgRating}</h3>
       <h4>{costForTwo}</h4>
     </div>
   );
